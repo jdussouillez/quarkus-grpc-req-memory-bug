632e8b6a353aefbe3b860c32611443bdbe6d015a
Junior DUSSOUILLEZ <junior.dussouillez@gmail.com>
2023-05-16 11:58:58 +0200

    chore: delay the PDF response

--- server/server-common/src/main/java/com/github/jdussouillez/server/service/PdfService.java
+++ server/server-common/src/main/java/com/github/jdussouillez/server/service/PdfService.java
@@ -4,6 +4,7 @@ import com.github.jdussouillez.server.Loggers;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 @ApplicationScoped
 public class PdfService {
@@ -12,6 +13,9 @@ public class PdfService {
         // TODO: generate real PDF content
         return Uni.createFrom().item(data)
             .invoke(d -> Loggers.MAIN.info("Data for generation: {}", d))
-            .map(d -> d.getBytes(StandardCharsets.UTF_8));
+            .map(d -> d.getBytes(StandardCharsets.UTF_8))
+            .onItem()
+            .delayIt()
+            .by(Duration.ofSeconds(2L));
     }
 }
